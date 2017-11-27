5d4a30c3f0a86616fa0508dbafaa1b82fa3149de
Patrick Davis <patrick@thepatrickdavis.com>
2017-11-27 14:17:51 -0500

    Added in stock calculation to inventory post

--- app/routes/inventory-routes.js
+++ app/routes/inventory-routes.js
@@ -47,12 +47,13 @@ module.exports = function(app, db) {
     app.post('/inventory', (req, res) => {
         let errors = [];
         let partId = null;
+        let part = null;
         // A parent part id is required for inventory quantities to be added
         if(!req.body.partId) {
             errors.push('Part Id Is Required');
         } else {
             partId = Number(req.body.partId);
-            const part = db.parts.findOne({id: partId});
+            part = db.parts.findOne({id: partId});
             if(!part) {
                 errors.push('Part Id \'' + partId + '\' does not exist.');
             }
@@ -75,6 +76,18 @@ module.exports = function(app, db) {
                 isActive: true };
 
             db.inventory.save(inventory);
+            // Upate the in stock quantity on the part
+            const inStock = Number((part.inStock || 0) + inventory.quantity);
+            const query = {
+                id: Number(inventory.partId)
+            };
+
+            var options = {
+                multi: false,
+                upsert: false
+             };
+            db.parts.update(query, {inStock: inStock}, options);
+
             res.send(inventory);
         }
     });
@@ -83,6 +96,7 @@ module.exports = function(app, db) {
         const id = req.params.id;
         let inventory = null;
         let errors = [];
+        let quantityDelta = 0;
         if(!id) {
             errors.push('Id is required.');
         }   else {
@@ -97,8 +111,8 @@ module.exports = function(app, db) {
             errors.push('Description is Required');
         }
 
-        if(!req.body.cost) {
-            errors.push('Cost is Required');
+        if(!req.body.quantity) {
+            errors.push('Quantity is Required');
         }
 
         if(errors.length > 0) {
@@ -117,6 +131,19 @@ module.exports = function(app, db) {
             inventory.description = req.body.description;
             inventory.cost = Number(req.body.cost);
             db.inventory.update(query, inventory, options);
+
+            quantityDelta = Number(req.body.quantity) - (inventory.quantity || 0);
+            if(quantityDelta != 0) {
+                // Need to update in stock quantity to reflect inventory change
+                partId = Number(inventory.partId);
+                const part = db.parts.findOne({id: partId});
+                if(!part) {
+                    errors.push('Part Id \'' + partId + '\' does not exist.');
+                } else {
+                    part.inStock = (part.inStock || 0) + quantityDelta;
+                }
+            }
+
             res.send(inventory);
         }
     });
