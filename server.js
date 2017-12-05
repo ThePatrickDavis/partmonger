1020b7983cb3e4164f3da0e50071162f1980c25d
Patrick Davis <patrick@thepatrickdavis.com>
2017-12-05 13:28:16 -0500

    Added cors for all origins

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const db             = require('diskdb');
+const cors           = require('cors')
 const app            = express();
 
 const port = 9001;
 
+app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 db.connect('./db', ['parts', 'inventory']);
 require('./app/routes')(app, db);
 
 app.listen(port, () => {
   console.log('We are live on ' + port);
+  console.log('CORS enabled on all origins');
 });
